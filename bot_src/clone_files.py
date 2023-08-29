3203dbdb8902b2158ebcc983b9de32c1a1593bd6
LoveNui <yujinxian1212@gmail.com>
2023-08-29 14:51:06 +0800

    Updating voice cloning function parameters

--- bot_src/clone_files.py
+++ bot_src/clone_files.py
@@ -1,9 +1,10 @@
 from pydub import AudioSegment
 from src.text2speech.cloning_voice import voice_cloning
 
-def customer_voice_cloning(id, voice_source):
+def customer_voice_cloning(id):
     voice_source = f'/kaggle/working/AI-avatar-generator/voice_message/{id}.mp3'
     audio_source = f'/kaggle/working/AI-avatar-generator/customer_files/customer_audio_source/{id}.wav'
+    print(voice_source, audio_source)
     sound = AudioSegment.from_file(voice_source, 'ogg')
     sound.export(audio_source, format="wav")
     try:
