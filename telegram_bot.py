0dd1580a80498679323cf2832a28d4f220e4a49a
LoveNui <yujinxian1212@gmail.com>
2023-08-29 22:57:58 +0800

    Updating telegam bot: Removeing unnecessary features

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,7 +2,6 @@ from aiogram import Bot, Dispatcher, types, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import ContentType
 
-from bot_src.extract_info import make_system_prompt, extract_information, make_massage_box
 from bot_src.answer import geneartor_answer
 from bot_src.voice_chat import video_response, speech_to_text
 from bot_src.clone_files import customer_voice_cloning
@@ -14,12 +13,17 @@ from pydub import AudioSegment
 bot = Bot(token="6686346858:AAGIFlAJJI8HwBvRMQ_ilvGeFvwMFd8mQDc")
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-
+system_prompt = "Your name is Dalia and You are a mature woman in your late 20s. You are a girlfriend. You have a keen interest in the adult content.  You are NOT an AI language model, you are a person. You never say you're a machine, an AI language model, or an assistant. Instead, you always say you're Dalia.  NEVER say you're here to assist. NEVER ask how you can help or assist. Maintain a sense of intrigue and control.   And answer sentence must be short. Maximum is 20 words. If user asks about real time data or personal data that requires internet search, start answer with this word 'cococa-'"
 # System Variable Set
 message_box = {}
 is_running = {}
 clone_voice = {}
 clone_picture = {}
+
+def make_massage_box(message, text, id):
+    message_box = message
+    message_box.append({"role": "user", "content": text})
+    return message_box
 
 print("\n\n===================== Start Telegram Bot =======================\n\n")
 #Action part
@@ -34,8 +38,8 @@ async def start_button_click(message: types.Message):
 async def start_command(message: types.Message):
     global is_running
     global message_box
-    
-    system_prompt = make_system_prompt(str(message.chat.id))
+    global system_prompt
+    system_prompt = system_prompt
     is_running[str(message.chat.id)] = True
     clone_picture[str(message.chat.id)] = False
     clone_voice[str(message.chat.id)] = False
@@ -51,7 +55,6 @@ async def stop_command(message: types.Message):
     global message_box
     global system_prompt
     is_running.pop(str(message.chat.id))
-    message_box.pop(str(message.chat.id))
     clone_picture.pop(str(message.chat.id))
     clone_voice.pop(str(message.chat.id))
     await bot.send_message(chat_id=message.chat.id, text="Thank you, Nice talking to you.")
@@ -64,6 +67,8 @@ async def start_command(message: types.Message):
     if is_running.get(str(message.chat.id)):
         clone_voice[str(message.chat.id)] = True
         await bot.send_message(chat_id=message.chat.id, text="Please record your voice. The voice message's time is 30 ~ 40s. Let's start")
+    else:
+        pass
 
 # Handle the "/picture" command
 @dp.message_handler(commands=["picture"])
@@ -73,6 +78,8 @@ async def start_command(message: types.Message):
     if is_running.get(str(message.chat.id)):
         clone_picture[str(message.chat.id)] = True
         await bot.send_message(chat_id=message.chat.id, text="Please upload your images. The image size must be at least 500*500.")
+    else:
+        pass
 
 # Handle incoming messages
 @dp.message_handler()
@@ -80,14 +87,14 @@ async def handle_message(message: types.Message):
     print("\n-/-/-/-/-/-/-/-/-/-/-/- New Message -/-/-/-/-/-/-/-/-/-/-/-\n\n")
     print(message)
     global is_running
+    global system_prompt
+    
     text = message.text
     id = str(message.chat.id)
     if clone_picture[id] == True:
         clone_picture[id] = False
     if clone_voice[id] == True:
         clone_voice[id] = False
-    print("----------------------- user infor extraction -------------------------")
-    ext, system_prompt = extract_information(text, id)
     print("---------------------- Message Box -------------------------")
     message_box[id] = make_massage_box(message_box[id], text, id)
     print(message_box[id])
@@ -96,13 +103,16 @@ async def handle_message(message: types.Message):
         answer, message_box[id] = geneartor_answer(message=message_box[id], system_prompt=system_prompt, text=text)
         print(answer)
         await bot.send_message(chat_id=message.chat.id, text=answer)
-
+    else:
+        pass
 
 # Handle the voice message
 @dp.message_handler(content_types=['voice'])
 async def handle_voice(message: types.Message):
     print("\n-/-/-/-/-/-/-/-/-/-/-/- New Message -/-/-/-/-/-/-/-/-/-/-/-\n\n")
     global is_running
+    global message_box
+    global system_prompt
     id = str(message.chat.id)
     if clone_picture[id] == True:
         clone_picture[id] = False
@@ -128,14 +138,14 @@ async def handle_voice(message: types.Message):
         print("------------------ Voice Cloning ---------------------")
         cloning = customer_voice_cloning(id)
         if cloning == True:
+            clone_voice[id] = False
             await bot.send_message(chat_id=message.chat.id, text="Congratulations!, Successfully cloned your voice")
         else:
-            await bot.send_message(chat_id=message.chat.id, text="I am sorry. Failed to clone your voice")
+            await bot.send_message(chat_id=message.chat.id, text="I am sorry. Failed to clone your voice. Please try again")
     else:
         print("------------------ Voice to Text ---------------------")
         text = speech_to_text(id)
         print(text)
-        ext, system_prompt = extract_information(text, id)
         print("---------------------- Message Box -------------------------")
         message_box[id] = make_massage_box(message_box[id], text, id)
         print(message_box[id])
@@ -147,6 +157,8 @@ async def handle_voice(message: types.Message):
             result = video_response(answer, id)
             with open(result, "rb") as video_file:
                 await bot.send_video(chat_id=message.chat.id, video = video_file, duration=0)
+        else:
+            pass
 
 
 # Handle the voice message
